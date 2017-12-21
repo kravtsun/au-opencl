61a414cb62fa7adac0dc99d5c7eb1ac1dfb5635e
Andrey Kravtsun <avkravtsun@gmail.com>
2017-12-21 21:25:37 +0300

    Cleaning.

--- prefix_sum/main.cpp
+++ prefix_sum/main.cpp
@@ -43,10 +43,13 @@ int closest_power_of_two(const int x)
     return 1 << i;
 }
 
-#define STRINGIFY(x) #x
-#define BLOCK_SIZE 256
+#define xstr(s) str(s)
+#define str(s) #s
+#define SCAN_BLOCK_SIZE 256
+#define TYPE double
+#define TYPE_SIZE sizeof(TYPE)
 
-using vector = std::vector<int>;
+using vector = std::vector<TYPE>;
 
 void profile_events(const cl::Event &start_event, const cl::Event finish_event, const std::string &s = "Total time: ")
 {
@@ -77,7 +80,8 @@ vector solve(const vector &v)
     cl::Program program(context, source);
     try
     {
-        program.build(devices, "-D BLOCK_SIZE=" STRINGIFY(BLOCK_SIZE));
+        const std::string build_str = "-D BLOCK_SIZE=" + std::to_string(SCAN_BLOCK_SIZE);
+        program.build(devices, build_str.c_str());
     }
     catch (cl::Error &ignored)
     {
@@ -87,74 +91,45 @@ vector solve(const vector &v)
         throw;
     }
 
-    const size_t double_size = sizeof(vector::value_type);
-    const int N = std::max(BLOCK_SIZE, closest_power_of_two(n));
+    const int N = std::max(SCAN_BLOCK_SIZE, closest_power_of_two(n));
 
-    cl::Buffer dev_input(context, CL_MEM_READ_ONLY, double_size * N);
-    cl::Buffer dev_output(context, CL_MEM_READ_WRITE, double_size * N);
+    cl::Buffer dev_input(context, CL_MEM_READ_ONLY, TYPE_SIZE * N);
+    cl::Buffer dev_output(context, CL_MEM_READ_WRITE, TYPE_SIZE * N);
 
-    //cl::Buffer a(context, CL_MEM_READ_WRITE, double_size * BLOCK_SIZE);
-    //cl::Locab(context, CL_MEM_READ_WRITE, double_size * BLOCK_SIZE);
+    auto a = cl::Local(TYPE_SIZE * SCAN_BLOCK_SIZE);
+    auto b = cl::Local(TYPE_SIZE * SCAN_BLOCK_SIZE);
 
-    auto a = cl::Local(double_size * BLOCK_SIZE);
-    auto b = cl::Local(double_size * BLOCK_SIZE);
+    queue.enqueueWriteBuffer(dev_input, CL_TRUE, 0, TYPE_SIZE * v.size(), v.data());
 
-    // copy from cpu to gpu
-    queue.enqueueWriteBuffer(dev_input, CL_TRUE, 0, double_size * v.size(), v.data());
-
-    cl::EnqueueArgs global_args{ queue, cl::NullRange, cl::NDRange(N), cl::NDRange(BLOCK_SIZE) };
+    cl::EnqueueArgs global_args{ queue, cl::NullRange, cl::NDRange(N), cl::NDRange(SCAN_BLOCK_SIZE) };
 
     auto bottom_up = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::LocalSpaceArg, cl::LocalSpaceArg>(program, "bottom_up");
     auto start_event = bottom_up(global_args, dev_input, dev_output, a, b);
     start_event.wait();
     profile_events(start_event, start_event, "bottom_up: ");
 
-    //vector tmp(n);
-    //queue.enqueueReadBuffer(dev_output, CL_TRUE, 0, double_size * n, tmp.data());
-
     auto reduce_blocks = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::LocalSpaceArg, cl::LocalSpaceArg, uint>(program, "reduce_blocks");
-    const int NBLOCKS = std::max(N / BLOCK_SIZE, BLOCK_SIZE);
-    cl::Buffer block_output(context, CL_MEM_READ_WRITE, double_size * NBLOCKS);
-    cl::EnqueueArgs block_args{ queue, cl::NullRange, cl::NDRange(BLOCK_SIZE), cl::NDRange(BLOCK_SIZE) };
+    const int NBLOCKS = std::max(N / SCAN_BLOCK_SIZE, SCAN_BLOCK_SIZE);
+    cl::Buffer block_output(context, CL_MEM_READ_WRITE, TYPE_SIZE * NBLOCKS);
+    cl::EnqueueArgs block_args{ queue, cl::NullRange, cl::NDRange(SCAN_BLOCK_SIZE), cl::NDRange(SCAN_BLOCK_SIZE) };
     auto event = reduce_blocks(block_args, dev_output, block_output, a, b, N);
     event.wait();
     profile_events(event, event, "reduce_blocks: ");
-
-    //vector tmp_blocks(NBLOCKS);
-    //queue.enqueueReadBuffer(block_output, CL_TRUE, 0, double_size * NBLOCKS, tmp_blocks.data());
-    //return tmp_blocks;
 
     auto reduce_all = cl::KernelFunctor<cl::Buffer, cl::Buffer>(program, "reduce_all");
     auto finish_event = reduce_all(global_args, dev_output, block_output);
     finish_event.wait();
     profile_events(finish_event, finish_event, "bottom_up: ");
 
-    //vector tmp(N);
-    //queue.enqueueReadBuffer(dev_output, CL_TRUE, 0, double_size * N, tmp.data());
-
-    //return tmp;
-
     vector result(v.size());
-    queue.enqueueReadBuffer(dev_output, CL_TRUE, 0, double_size * v.size(), result.data());
+    queue.enqueueReadBuffer(dev_output, CL_TRUE, 0, TYPE_SIZE * v.size(), result.data());
     queue.finish();
     return result;
 }
 
 int main()
 {
-    //freopen("big_input.txt", "w", stdout);
-    //int N = 1048576;
-    //using std::cout;
-    //using std::endl;
-    //cout << N << endl;
-    //for (int i = 0; i < N; ++i)
-    //{
-    //    cout << "1 ";
-    //}
-    //cout << endl;
-    //return 0;
-
-    std::ifstream fin("big_input.txt");
+    std::ifstream fin("input.txt");
     int n;
     fin >> n;
     vector v(n);
@@ -163,37 +138,46 @@ int main()
         fin >> v[i];
     }
 
-    try
+    auto result = solve(v);
+    bool test_run = false;
+    if (!test_run)
     {
-        auto result = solve(v);
-        std::cout << "Result: " << std::endl;
-        //std::copy(result.begin(), result.end(), std::ostream_iterator<double>(std::cout, " "));
-        //std::cout << std::endl;
-        vector expected;
-        std::partial_sum(v.begin(), v.end(), std::back_inserter(expected));
-        if (result != expected)
+        std::ofstream fout("output.txt");
+        fout << std::fixed << std::setprecision(3);
+        std::copy(result.begin(), result.end(), std::ostream_iterator<TYPE>(fout, " "));
+    }
+    else {
+        try
         {
-            double s = 0.0;
-            for (int i = 0; i < n; ++i)
+            //std::cout << "Result: " << std::endl;
+            //std::copy(result.begin(), result.end(), std::ostream_iterator<double>(std::cout, " "));
+            //std::cout << std::endl;
+            vector expected;
+            std::partial_sum(v.begin(), v.end(), std::back_inserter(expected));
+            if (result != expected)
             {
-                if (i > 0 && result[i] != result[i - 1] + 1)
+                double s = 0.0;
+                for (int i = 0; i < n; ++i)
                 {
-                    std::cout << i + 1 << std::endl;
-                    break;
+                    if (i > 0 && result[i] != result[i - 1] + 1)
+                    {
+                        std::cout << i + 1 << std::endl;
+                        break;
+                    }
+                    s += std::abs(result[i] - expected[i]);
                 }
-                s += std::abs(result[i] - expected[i]);
-            }
 
-            std::cout << "Error: " << std::setprecision(2) << s << std::endl;
+                std::cout << "Error: " << std::setprecision(2) << s << std::endl;
+            }
         }
-    }
-    catch (cl::Error &e)
-    {
-        std::cout << "[OpenCL ERROR] " << e.what() << " : " << e.err() << std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cout << "[ERROR] " << e.what();
+        catch (cl::Error &e)
+        {
+            std::cout << "[OpenCL ERROR] " << e.what() << " : " << e.err() << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << "[ERROR] " << e.what();
+        }
     }
 
     return 0;
--- prefix_sum/prefix_sum.cl
+++ prefix_sum/prefix_sum.cl
@@ -1,13 +1,7 @@
-#define SWAP(a,b) {__local int * tmp=a; a=b; b=tmp;}
-#define TYPE int
-#define BLOCK_SIZE 256
+#define SWAP(a,b) {__local TYPE * tmp=a; a=b; b=tmp;}
+#define TYPE double
 
-uint get_worker_base(uint group_base, uint perworker_size, uint worker_index) {
-    return group_base + worker_index * perworker_size + 0;
-}
-
-// TODO a, b block_size!!!
-void scan_hillis_steele(__local int * a, __local int * b, uint lid)
+void scan_hillis_steele(__local TYPE * a, __local TYPE * b, uint lid)
 {
     uint block_size = get_local_size(0);
     for (uint s = 1; s < block_size; s <<= 1)
@@ -25,7 +19,7 @@ void scan_hillis_steele(__local int * a, __local int * b, uint lid)
     }
 }
 
-__kernel void bottom_up(__global int * input, __global int * output, __local int * a, __local int * b)
+__kernel void bottom_up(__global TYPE * input, __global TYPE * output, __local TYPE * a, __local TYPE * b)
 {
     uint gid = get_global_id(0);
     uint lid = get_local_id(0);
@@ -36,7 +30,7 @@ __kernel void bottom_up(__global int * input, __global int * output, __local int
 }
 
 // For one work group.
-__kernel void reduce_blocks(__global int * output, __global int * block_output, __local int * a, __local int * b, uint n)
+__kernel void reduce_blocks(__global TYPE * output, __global TYPE * block_output, __local TYPE * a, __local TYPE * b, uint n)
 {
     uint big_block_id = get_local_id(0);
     uint n_small_blocks = n / BLOCK_SIZE;
@@ -46,15 +40,15 @@ __kernel void reduce_blocks(__global int * output, __global int * block_output, 
         perworker_size = 1;
     }
 
-    int s = 0;
+    TYPE s = 0;
     for (uint i = 0; i < perworker_size; ++i)
     {
-        int gid = BLOCK_SIZE * perworker_size * big_block_id + i * BLOCK_SIZE - 1;
+        uint gid = BLOCK_SIZE * perworker_size * big_block_id + i * BLOCK_SIZE;
         if (gid > 0)
         {
-            s += output[gid];
+            s += output[gid - 1];
         }
-        int block_output_id = perworker_size * big_block_id + i;
+        uint block_output_id = perworker_size * big_block_id + i;
         block_output[block_output_id] = s;
     }
 
@@ -62,19 +56,19 @@ __kernel void reduce_blocks(__global int * output, __global int * block_output, 
     a[lid] = b[lid] = s;
     barrier(CLK_LOCAL_MEM_FENCE);
     scan_hillis_steele(a, b, lid);
-    int previous_blocks_sum = 0;
+    TYPE previous_blocks_sum = 0;
     if (lid > 0)
     {
         previous_blocks_sum = a[lid - 1];
     }
     for (uint i = 0; i < perworker_size; ++i)
     {
-        int block_output_id = perworker_size * big_block_id + i;
+        uint block_output_id = perworker_size * big_block_id + i;
         block_output[block_output_id] += previous_blocks_sum;
     }
 }
 
-__kernel void reduce_all(__global int * output, __global int * block_output)
+__kernel void reduce_all(__global TYPE * output, __global TYPE * block_output)
 {
     uint gid = get_global_id(0);
     uint block_id = gid / get_local_size(0);
